5ca6daa641ab89f48037bfb5c2f4fe0ab8ef1475
Stellpflug <Derreck.Stellpflug@hco.hc.hctx.net>
2017-04-05 09:54:51 -0500

    Working score calculator

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,6 +11,21 @@ export class HomePage {
 //Declare ngModels in this class to be used in the home.html page
   name1 = '';
   name2 = '';
+  //Manually hardcore a score
+  //score = 42;
+
+  //declare functions in home.ts always
+  get score(){
+    const letters = (this.name1 + this.name2).toLowerCase();
+    let sum = 0;
+    //Have to use let instead of in int foor loops with typescript
+    for(let i = 0; i < letters.length; i++)
+    {
+      sum += letters.charCodeAt(i);
+    }
+    //using modulus we can only get a value 0-100
+    return sum%101;
+  }
 
 //Used for navigating multiple pages
   constructor(public navCtrl: NavController) {
