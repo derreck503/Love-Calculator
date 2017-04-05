642d5495fa2c9f1b11a53c2873c3c48adf1c7582
Stellpflug <Derreck.Stellpflug@hco.hc.hctx.net>
2017-04-05 09:38:43 -0500

    Binded input to int values

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+//Necesaary to use navController at the bottom
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -8,6 +8,11 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+//Declare ngModels in this class to be used in the home.html page
+  name1 = '';
+  name2 = '';
+
+//Used for navigating multiple pages
   constructor(public navCtrl: NavController) {
     
   }
